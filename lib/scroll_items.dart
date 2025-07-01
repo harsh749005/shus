12d56face6ff97596a9379f94b0ba156d238d551
Harsh <patelharsh749005@gmail.com>
2025-07-01 17:08:47 +0530

    listview.builder

--- lib/scroll_items.dart
+++ lib/scroll_items.dart
@@ -1,36 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:inputfield/models/sqaure.dart';
 
-class ScrollItems extends StatefulWidget {
-  const ScrollItems({super.key});
+class ScrollItems extends StatelessWidget {
+  ScrollItems({super.key});
+  final List _post = ['post 1', 'post2', 'post3','post4'];
 
-  @override
-  ScrollItemsState createState() => ScrollItemsState();
-}
-
-class ScrollItemsState extends State<ScrollItems> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: 
-      Text("Scroll Page")
-      ),
-      body: ListView(
-        
-        children: [
-
-          Sqaure(),
-          Sqaure(),
-          Sqaure(),
-          Sqaure(),
-          Sqaure(),
-          Sqaure(),
-          Sqaure(),
-          Sqaure(),
-          Sqaure(),
-          Sqaure(),
-        ],
-
+      appBar: AppBar(title: Text("Scroll Page")),
+      body: ListView.builder(
+        itemCount: _post.length,
+        itemBuilder: (context, index) {
+          return Sqaure();
+        },
       ),
     );
   }
